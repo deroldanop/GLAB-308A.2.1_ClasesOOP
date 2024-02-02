2b626de709db67ae2b7a89c9033ed2e42b0db8f8
Roldan Ordaz Paez <roldanop@gmail.com>
2024-02-02 14:42:51 -0800

    class constructor

--- index.js
+++ index.js
@@ -25,3 +25,42 @@ const adventurer = {
   // Test the roll method
   adventurer.roll();
   adventurer.roll(5);
+
+    //Part 2: Class 
+
+    class Character {
+        constructor(name) {
+          this.name = name;
+          this.health = 100;
+          this.inventory = [];
+        }
+      
+        roll(mod = 0) {
+          const result = Math.floor(Math.random() * 20) + 1 + mod;
+          console.log(`${this.name} rolled a ${result}.`);
+        }
+      }
+      
+      class Adventurer extends Character {
+        constructor(name, role) {
+          super(name);
+          this.role = role;
+          this.inventory.push("bedroll", "50 gold coins");
+        }
+      
+        scout() {
+          console.log(`${this.name} is scouting ahead...`);
+          super.roll();
+        }
+      }
+      
+      const robin = new Adventurer("Robin", "Warrior");
+      robin.companion = new Adventurer("Leo", "Cat");
+      robin.companion.companion = new Adventurer("Frank", "Flea");
+      robin.companion.companion.inventory = ["small hat", "sunglasses"];
+      
+      // Testing the scout method
+      robin.scout();
+      robin.companion.scout();
+      robin.companion.companion.scout();
+    
